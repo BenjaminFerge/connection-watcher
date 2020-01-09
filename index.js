87fd63ae72d9eed5e57a5af000e817a410efd4d3
Benjámin J. Ferge <benjamin.ferge@protonmail.com>
2020-01-09 13:29:40 +0100

    Add basic functionalities

---
+++ index.js
@@ -0,0 +1,79 @@
+"use strict";
+
+const results = {};
+const watchers = {};
+
+function createReport(ok, lastResponse) {
+    return {
+        ok,
+        lastResponse,
+        timestamp: +new Date
+    };
+}
+
+async function checkConn(url, fetchOptions, openedCb, closedCb) {
+    return await fetch(url, fetchOptions)
+        .then(response => {
+            if (response.ok) {
+                const obj = createReport(true, response);
+                openedCb(obj);
+                return obj;
+            }
+            const obj = createReport(false, response);
+            closedCb(obj);
+            return obj;
+        })
+        .catch(err => {
+            const obj = createReport(false, err);
+            closedCb(obj)
+            return obj;
+        });
+}
+
+function setWatcher(id, conn) {
+    const {onOpened, onClosed, interval, url, fetchOptions} = conn;
+    const watcher = () => {
+        checkConn(url, fetchOptions, onOpened, onClosed)
+        .then(res => {
+            results[id] = {
+                ...res,
+                
+            }
+        });
+    }
+    const watcherId = window.setInterval(watcher, interval);
+    watchers[id] = watcherId;
+}
+
+function getResults() {
+    return results;
+}
+
+function stopById(id) {
+    window.clearInterval(id);
+}
+
+function stopByConnectionId(id) {
+    const watcherId = watchers[id];
+    stopById(watcherId);
+}
+
+function stopAll() {
+    Object.keys(watchers).forEach(id => stopByConnectionId(id));
+}
+
+function init(options) {
+    const {connections} = options;
+    Object.keys(connections).forEach(id => {
+        const conn = connections[id];
+        setWatcher(id, conn);
+    })
+}
+
+module.exports = {
+    init,
+    stopByConnectionId,
+    stopById,
+    stopAll,
+    getResults
+};
